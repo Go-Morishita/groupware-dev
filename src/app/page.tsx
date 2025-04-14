433058afdef4fb4fd4b85223111a0c6acc82faeb
Daichi <a5822108@aoyama.jp>
2025-04-14 17:21:45 +0900

    5: daichi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
+"use client"
 import { signIn } from "@/app/lib/auth";
-import { FcGoogle } from "react-icons/fc";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
+  const router = useRouter();
+  const toLogin = () => {
+    router.push("/login");
+  }
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 p-8">
       <h1 className="text-4xl font-bold mb-8">ROLEE</h1>
-      <button
-        onClick={async () => {
-          "use server";
-          await signIn("google", { redirectTo: "/home" });
-        }}
-        className="flex items-center gap-3 bg-white border border-gray-300 hover:shadow-lg rounded-md px-4 py-2 transition-shadow"
-      >
-        <FcGoogle size={24} />
-        <span className="text-lg">Sign in with Google</span>
-      </button>
+        <button
+          onClick={toLogin}
+          className="flex items-center gap-3 bg-white border border-gray-300 hover:shadow-lg rounded-md px-4 py-2 transition-shadow"
+        >
+          <span className="text-lg">ログイン</span>
+        </button>
     </div>
   );
 }
